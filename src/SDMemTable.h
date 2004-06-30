7bb8c5c04888dd007f1786a3c55d32bd9a46220c
mmarquar <mmarquar@bac3dc5d-e65c-4774-9384-1203941d114d>
2004-06-30 06:44:24 +0000

    Added documentation.

--- src/SDMemTable.h
+++ src/SDMemTable.h
@@ -49,57 +49,83 @@ class SDFrequencyTable;
 
 class SDMemTable {
 public:
+  // create a new (empty) SDMemTable
   SDMemTable();
+  // create a SDMemTable from a )aips++) table on disk
   SDMemTable(const std::string& name);
+
+  // Copy Construct a SDMemTable, if clear==True only header and
+  // skeleton are copied, otherwise the whole table is copied.
   SDMemTable(const SDMemTable& other, Bool clear=False);
 
+  // Copy Construct a SDMemTable, give a scanid constraint
+  // see also getScan()
   SDMemTable(const Table& tab, Int scanID);
+
   virtual ~SDMemTable();
+
+  // put data from meta conatiner into the table
   virtual bool putSDContainer(const SDContainer& sdc);
   virtual bool putSDHeader(const SDHeader& sdh);
   virtual bool putSDFreqTable(const SDFrequencyTable& sdft) {;}
   
+  // get spectrum,mask and tsys for the given row, at the selected
+  // cursor
   virtual std::vector<float> getSpectrum(Int whichRow) const;
   virtual std::vector<bool> getMask(Int whichRow) const;
-  
-  MaskedArray<Float> rowAsMaskedArray(uInt whichRow,
-				      Bool useSelection = False);
-
   virtual Float getTsys(Int whichRow) const;
+
+  // get info fro current row
   virtual Double getTime(Int whichRow) const ;
-  virtual std::string getSourceName(Int whichRow) const ;
+  virtual std::string getSourceName(Int whichRow) const;
   
+  // set the current value
   virtual bool setIF(Int whichIF=0);
   virtual bool setBeam(Int whichBeam=0);
   virtual bool setPol(Int whichPol=0);    
   //sets the user mask
   virtual bool setMask(const std::vector<int>& whichChans);
  
-
+  // return the currently selected values
   virtual Int getIF() { return IFSel_; }
   virtual Int getBeam() { return beamSel_; }
   virtual Int getPol() { return polSel_; }   
  
+  // print a summary to stdout
   virtual void summary() const;
   
+  // the (irrelevant) name 
   std::string name() const;
+
+  // write to disk as aips++ table
   void makePersistent(const std::string& filename);
+
+  // get a new SDMemTable containg all rows with the same give SCANID
   SDMemTable getScan(Int scanID);
 
   const TableRecord& getHeader() const {;}
+  // get a handle to the "raw" aips++ table
   const Table& table() { return table_; }
 
+  // return the number of values
   Int nBeam() const;
   Int nIF() const;
   Int nPol() const;
   Int nChan() const;
 
-private:
-  void setup();
+  // return a row as a Masked array, internally converting uChar flags
+  // to bool mask
+  MaskedArray<Float> rowAsMaskedArray(uInt whichRow,
+				      Bool useSelection = False);
 
+private:
+  // set up tabel structure
+  void setup();
+  // the current cursor into the array
   Int IFSel_,beamSel_,polSel_;
-  std::vector<bool> chanMask_;
+  std::vector<bool> chanMask_;  
   String name_;
+  // the unerlying memory table
   Table table_;
 };
 
