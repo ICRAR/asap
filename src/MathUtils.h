24638ecc80583d39682ed8c159aef27c50821812
mmarquar <mmarquar@bac3dc5d-e65c-4774-9384-1203941d114d>
2004-07-07 08:44:30 +0000

    Various Vector functions not present in aips++. Maybe move them there later. Hanning the only one for now.

---
+++ src/MathUtils.h
@@ -0,0 +1,42 @@
+//#---------------------------------------------------------------------------
+//# MathUtilities.h: General math operations
+//#---------------------------------------------------------------------------
+//# Copyright (C) 2004
+//# Malte Marquarding, ATNF
+//#
+//# This program is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU General Public License as published by the Free
+//# Software Foundation; either version 2 of the License, or (at your option)
+//# any later version.
+//#
+//# This program is distributed in the hope that it will be useful, but
+//# WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+//# Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with this program; if not, write to the Free Software Foundation, Inc.,
+//# 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning this software should be addressed as follows:
+//#        Internet email: Malte.Marquarding@csiro.au
+//#        Postal address: Malte Marquarding,
+//#                        Australia Telescope National Facility,
+//#                        P.O. Box 76,
+//#                        Epping, NSW, 2121,
+//#                        AUSTRALIA
+//#
+//# $Id:
+//#---------------------------------------------------------------------------
+#ifndef _MATHUTILS_H_
+#define _MATHUTILS_H_
+
+#include <aips/aips.h>
+#include <aips/Arrays/Vector.h>
+
+template <class T>
+void hanning(Vector<T>& out, Vector<Bool>& outmask, 
+	     const Vector<T>& in, const Vector<Bool>& mask, 
+	     Bool relaxed=False, Bool ignoreOther=False);
+
+#endif
