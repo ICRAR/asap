1337edc36550a20ef51706c9f43aa9f874814221
mar637 <mar637@bac3dc5d-e65c-4774-9384-1203941d114d>
2004-11-12 02:42:00 +0000

    pyhton wrapper for SDFitter

---
+++ src/python_SDFitter.cc
@@ -0,0 +1,62 @@
+//#---------------------------------------------------------------------------
+//# python_SDFitter.cc: python exposure of c++ SDFitter class
+//#---------------------------------------------------------------------------
+//# Copyright (C) 2004
+//# Malte Marquarding, ATNF
+//#
+//# This program is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU General Public License as published by the Free
+//# Software Foundation; either version 2 of the License, or (at your option)
+//# any later version.
+//#
+//# This program is distributed in the hope that it will be useful, but
+//# WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+//# Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with this program; if not, write to the Free Software Foundation, Inc.,
+//# 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning this software should be addressed as follows:
+//#        Internet email: Malte.Marquarding@csiro.au
+//#        Postal address: Malte Marquarding,
+//#                        Australia Telescope National Facility,
+//#                        P.O. Box 76,
+//#                        Epping, NSW, 2121,
+//#                        AUSTRALIA
+//#
+//# $Id:
+//#---------------------------------------------------------------------------
+#include <boost/python.hpp>
+
+#include "SDFitter.h"
+
+using namespace boost::python;
+
+namespace asap {
+  namespace python {
+
+    void python_SDFitter() {
+      class_<SDFitter>("fitter")
+        .def( init <> () )
+        .def("setexpression", &SDFitter::setExpression)
+        .def("setdata", &SDFitter::setData)
+        .def("getresidual", &SDFitter::getResidual)//,
+             //(boost::python::arg("which")=-1) )
+        .def("getfit", &SDFitter::getFit)
+        .def("getfixedparameters", &SDFitter::getFixedParameters)
+        .def("setfixedparameters", &SDFitter::setFixedParameters)
+        .def("getparameters", &SDFitter::getParameters)
+        .def("setparameters", &SDFitter::setParameters)
+        .def("getestimate", &SDFitter::getEstimate)
+        .def("estimate", &SDFitter::computeEstimate)
+        .def("geterrors", &SDFitter::getErrors)
+        .def("getchi2", &SDFitter::getChisquared)
+        .def("reset", &SDFitter::reset)
+        .def("fit", &SDFitter::fit)
+      ;
+    };
+
+  } //namespace python
+} // namespace asap
