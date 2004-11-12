aebff99bf9ece88ee6bdf8333195a88348dbacb2
mar637 <mar637@bac3dc5d-e65c-4774-9384-1203941d114d>
2004-11-12 01:11:13 +0000

    changes summary to return std::string comment out unused getMask function rename a few functions to make it consistent nXXX() added nCoordinates

--- src/SDMemTable.h
+++ src/SDMemTable.h
@@ -83,35 +83,42 @@ public:
   virtual Float getTsys(Int whichRow=0) const;
   // get all as aips++ Vectors
   virtual void getSpectrum(Vector<Float>& spectrum, Int whichRow=0);
-  virtual void getMask(Vector<Bool>& mask,Int whichRow=0) const;
+
+  //virtual void getMask(Vector<Bool>& mask,Int whichRow=0) const;
 
   // get info for current row
   std::string getTime(Int whichRow=0) const ;
   std::string getSourceName(Int whichRow=0) const;
   double getInterval(Int whichRow=0) const;
 
+  virtual void setSpectrum(std::vector<float> spectrum, int whichRow=0);
+  virtual void setRestFreqs(std::vector<double> freqs, const std::string& theunit);
+
   // set the current value
   virtual bool setIF(Int whichIF=0);
   virtual bool setBeam(Int whichBeam=0);
-  virtual bool setPol(Int whichPol=0);    
-  //sets the user mask
+  virtual bool setPol(Int whichPol=0);
+
+  //sets the user mask applied to all spectra
   virtual bool setMask(std::vector<int> whichChans);
- 
+  // Hard flags the current spectrum, not reversible
+  virtual void flag(int whichRow);
+
   // return the currently selected values
   virtual Int getIF() { return IFSel_; }
   virtual Int getBeam() { return beamSel_; }
-  virtual Int getPol() { return polSel_; }   
- 
+  virtual Int getPol() { return polSel_; }
+
   // number of scans in table
-  virtual Int nScans() const;
+  virtual Int nScan() const;
 
   // print a summary to stdout
-  virtual void summary() const;
-  
+  virtual std::string summary() const;
+
   // write to disk as aips++ table
   void makePersistent(const std::string& filename);
 
-  // get a new SDMemTable containg all rows with the same give SCANID
+  // get a new SDMemTable containing all rows with the same give SCANID
   SDMemTable getScan(Int scanID);
   SDMemTable getSource(const std::string& source);
 
@@ -126,26 +133,28 @@ public:
   Int nChan() const;
 
   // return the number of rows (integrations) in the table
-  Int nRows() const { return table_.nrow(); }
+  Int nRow() const { return table_.nrow(); }
 
   // return a row as a Masked array, internally converting uChar flags
   // to bool mask
   MaskedArray<Float> rowAsMaskedArray(uInt whichRow,
-				      Bool useSelection = False);
+                                      Bool useSelection = False);
 
   SpectralCoordinate getCoordinate(uInt whichIdx) const;
   Bool setCoordinate(const SpectralCoordinate& speccord, uInt whichIdx);
 
-  std::vector<double> getAbscissa(int whichRow, 
-				  const std::string& whichUnit="GHz",
-				  const std::string& whichFrame="TOPO",
-				  double restfreq=0.0);
+  Int nCoordinates() const;
+
+  std::vector<double> getAbscissa(int whichRow,
+                                  const std::string& whichUnit="GHz",
+                                  const std::string& whichFrame="TOPO",
+                                  double restfreq=0.0);
 private:
   // set up table structure
   void setup();
   // the current cursor into the array
   Int IFSel_,beamSel_,polSel_;
-  std::vector<bool> chanMask_;  
+  std::vector<bool> chanMask_;
   // the underlying memory table
   Table table_;
 };
