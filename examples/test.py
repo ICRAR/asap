f0123fce52b2eee4b455702f356fa988f69ebc93
mar637 <mar637@bac3dc5d-e65c-4774-9384-1203941d114d>
2004-12-01 01:30:51 +0000

    Aligned with asap v0.1a

--- examples/test.py
+++ examples/test.py
@@ -2,49 +2,37 @@
 import os
 #import asap module
 from asap import *
-# create a reader
-r = sdreader()
-# open an RPFITS file
-r.open('/u/mmarquar/zorro/singledish/data/2001-09-01_0332_P363.rpf') 
+# create a readersa and open an RPFITS file
+r = reader('/u/mmarquar/zorro/singledish/data/2001-09-01_0332_P363.rpf')
 # create a vector with numbers [0..109]
 integrations = range(110)
-r.read(integrations)
 # get the data out of  the reader
-scans = r.getdata()
+scans = r.read(integrations)
 # close the reader
 r = None
-# Test sdwriter.
-print 'Writing ASAP table to disk as /tmp/test.tbl...'
-scans.makepersistent('/tmp/test.tbl')
-print "removing /tmp/test.tbl ..."
-os.remove('/tmp/test_SDWriter.sdfits')
-print 'Begin sdwriter tests...'
-# Create an MS2 writer.
-w = sdwriter('MS2')
-# Change to SDFITS output format (the default).
-w.setformat()
-# Write out the spectra.
-w.write(scans, '/tmp/test_SDWriter.sdfits')
+print 'Begin export test...'
+# Write out the spectra in SDFITS format
+scans.save('/tmp/test_SDWriter.sdfits','SDFITS')
 # clean up
 print "removing test_SDWriter.sdfits ..."
 os.remove('/tmp/test_SDWriter.sdfits')
 # print a short summary of the data
 scans.summary()
-# get the scan with the number '1'
-scan = scans.getscan(0)
-# get the scan with the number '1'
-ref = scans.getscan(1)
+# get the scan with the number '0'
+scan = scans.get_scan(0)
+# get the scan with the name 'ref_R'
+ref = scans.get_scan('ref_R')
 # close the data table
 scans = None
 # open the math server
 # average the "on" scan
-scanav = average(scan)
+scanav = average_time(scan)
 # get rid of the original scan
 scan = None
 # print a summary of the scan
 scanav.summary()
 # average the "off" scan
-refav = average(ref)
+refav = average_time(ref)
 # get rid of the original scan
 ref = None
 # print a summary of the scan
@@ -52,20 +40,20 @@ refav.summary()
 # form the quotione spectrum
 quot = quotient(scanav,refav)
 # set the cursor to polarisation 0
-quot.setpol(0)
+quot.set_selection(thepol=0)
 # get the spectrum for polarisation 0
 v0 = quot.getspectrum()
 #print  the first ten channel
 print v0[0:10]
 # set the cursor to polarisation 1
-quot.setpol(1)
+quot.set_selection(thepol=1)
 # get the spectrum for polarisation 1
 v1 = quot.getspectrum()
 #print  the first ten channel
 print v1[0:10]
 # write it to disk for further use
-quot.makepersistent('/tmp/myfirstquotient.tbl')
+quot.save('/tmp/myfirstquotient.asap')
 # cleanup
-print "removing /tmp/myfirstquotient.tbl ..."
-os.system('rm -rf /tmp/myfirstquotient.tbl')
+print "removing /tmp/myfirstquotient.asap ..."
+os.system('rm -rf /tmp/myfirstquotient.asap')
 print "Test successful."
