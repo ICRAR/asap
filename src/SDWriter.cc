9f48b9e8bce431587eaa62330c590d8015eff24b
kil064 <kil064@bac3dc5d-e65c-4774-9384-1203941d114d>
2005-01-30 07:01:46 +0000

    the majority of the frequency information for each row was not being written out.  make some effort to
    1) implement this
    2) get it right - not really sure about some of the assumption of the
     underlying writer

--- src/SDWriter.cc
+++ src/SDWriter.cc
@@ -39,6 +39,9 @@
 #include <atnf/PKSIO/PKSMS2writer.h>
 #include <atnf/PKSIO/PKSSDwriter.h>
 
+#include <tables/Tables/ArrayColumn.h>
+
+
 #include "SDContainer.h"
 #include "SDMemTable.h"
 #include "SDWriter.h"
@@ -106,7 +109,7 @@ Int SDWriter::setFormat(const std::string &format)
 // Write an SDMemTable to file in the desired format, closing the file when
 // finished.
 
-Int SDWriter::write(const CountedPtr<SDMemTable> table,
+Int SDWriter::write(const CountedPtr<SDMemTable> in,
                     const std::string &filename)
 {
 
@@ -115,14 +118,14 @@ Int SDWriter::write(const CountedPtr<SDMemTable> table,
   if (cFormat=="FITS") {
      Bool verbose = True;
      SDFITSImageWriter iw;
-     if (iw.write(*table, filename, verbose)) {
+     if (iw.write(*in, filename, verbose)) {
         return 0;
      } else {
         return 1;
      }
   } else if (cFormat=="ASCII") {
      SDAsciiWriter iw;
-     if (iw.write(*table, filename)) {
+     if (iw.write(*in, filename)) {
         return 0;
      } else {
         return 1;
@@ -132,9 +135,27 @@ Int SDWriter::write(const CountedPtr<SDMemTable> table,
 // MS or SDFITS
 
   // Extract the header from the table.
-  SDHeader hdr = table->getSDHeader();
-  Int nPol  = hdr.npol;
-  Int nChan = hdr.nchan;
+  SDHeader hdr = in->getSDHeader();
+  const Int nPol  = hdr.npol;
+  const Int nChan = hdr.nchan;
+
+// Get Freq table
+
+  SDFrequencyTable sdft = in->getSDFreqTable();
+  Vector<Double> restFreqs;
+  String restFreqUnit;
+  sdft.restFrequencies(restFreqs, restFreqUnit);
+  Double restFreq = 0.0;
+  if (restFreqs.nelements()>0) {
+     Quantum<Double> rF(restFreqs(0), Unit(restFreqUnit));
+     restFreq = rF.getValue(Unit("Hz"));
+  }
+
+// Table columns
+
+  const Table table = in->table();
+  ROArrayColumn<uInt> freqIDCol(table, "FREQID");
+  Vector<uInt> freqIDs;
 
   // Create the output file and write static data.
   Int status;
@@ -151,9 +172,9 @@ Int SDWriter::write(const CountedPtr<SDMemTable> table,
   Double mjd0 = 0.0;
 
   Int count = 0;
-  for (Int iRow = 0; iRow < table->nRow(); iRow++) {
+  for (Int iRow = 0; iRow < in->nRow(); iRow++) {
     // Extract the next integration from the table.
-    SDContainer sd = table->getSDContainer(iRow);
+    SDContainer sd = in->getSDContainer(iRow);
     if (sd.scanid != scanNo) {
       scanNo = sd.scanid;
       mjd0 = sd.timestamp;
@@ -162,18 +183,31 @@ Int SDWriter::write(const CountedPtr<SDMemTable> table,
       cycleNo++;
     }
 
+    // Get FreqID vector
+    freqIDCol.get(iRow, freqIDs);
+
     // Write it out beam by beam.
     for (Int iBeam = 0; iBeam < hdr.nbeam; iBeam++) {
 
       // Write it out IF by IF.
       for (Int iIF = 0; iIF < hdr.nif; iIF++) {
+        uInt freqID = freqIDs(iIF);
+
         // None of these are stored in SDMemTable by SDReader.
         //String          fieldName = "";
         //Vector<Double>  srcDir(2, 0.0);
         Vector<Double>  srcPM(2, 0.0);
         Double          srcVel = 0.0;
-        Double          freqInc = 0.0;
-        Double          restFreq = 0.0;
+
+// The writer will assume refPix = nChan/2 + 1.  So recompute
+// the frequency at this location. 
+
+        Double          cdelt = sdft.increment(freqID);
+        Double          crval = sdft.referenceValue(freqID);
+        Double          crpix = sdft.referencePixel(freqID);
+        Double          pixel = nChan/2 + 1;
+        Double          refFreqNew = (pixel-crpix)*cdelt + crval;
+//
         //Vector<Float>   tcal(2, 0.0f);
         //String          tcalTime = "";
         //Float           azimuth = 0.0f;
@@ -196,11 +230,12 @@ Int SDWriter::write(const CountedPtr<SDMemTable> table,
         Matrix<Float>   baseSub(nPol,9, 0.0f);
         Complex         xCalFctr;
         Vector<Complex> xPol;
+//
         if (status = cWriter->write(sd.scanid, cycleNo, sd.timestamp,
                                     sd.interval, sd.fieldname, sd.sourcename,
                                     sd.getDirection(iBeam),
-                                    srcPM, srcVel, iIF+1, hdr.reffreq,
-                                    hdr.bandwidth, freqInc, restFreq, sd.tcal,
+                                    srcPM, srcVel, iIF+1, refFreqNew,
+                                    nChan*abs(cdelt), cdelt, restFreq, sd.tcal,
                                     sd.tcaltime, sd.azimuth, sd.elevation, 
 				    sd.parangle,
                                     focusAxi, focusTan, focusRot, temperature,
