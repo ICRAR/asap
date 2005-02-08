92dd869135404c53b49ddae6e261bd0ed44d66c5
kil064 <kil064@bac3dc5d-e65c-4774-9384-1203941d114d>
2005-02-08 02:20:50 +0000

    doc only

--- src/MathUtils.h
+++ src/MathUtils.h
@@ -40,7 +40,7 @@ namespace mathutil {
 template <class T>
 casa::uInt addEntry (casa::Vector<T>& list, T val);
 
-// FInd the scan boundaries from a list of ScanIDs
+// Find the scan boundaries from a list of ScanIDs
 void scanBoundaries (casa::Vector<casa::uInt>& startInt,  
                      casa::Vector<casa::uInt>& endInt,
                      const casa::Vector<casa::Int>& scanIDs);
