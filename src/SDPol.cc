cec940b1b8edacf13d87f3ee64031548971a4669
kil064 <kil064@bac3dc5d-e65c-4774-9384-1203941d114d>
2005-02-14 01:36:08 +0000

    classes to do some polarimetric processing initially
      SDPolUtil (conversion utilities)
      SDStokesENgine (virtual STOKES column)

---
+++ src/SDPol.cc
@@ -0,0 +1,248 @@
+//#---------------------------------------------------------------------------
+//# SDPol.cc: Polarimetric functionality
+//#---------------------------------------------------------------------------
+//# Copyright (C) 2004
+//# ATNF
+//#
+//# This program is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU General Public License as published by the Free
+//# Software Foundation; either version 2 of the License, or (at your option)
+//# any later version.
+//#
+//# This program is distributed in the hope that it will be useful, but
+//# WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+//# Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with this program; if not, write to the Free Software Foundation, Inc.,
+//# 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning this software should be addressed as follows:
+//#        Internet email: Malte.Marquarding@csiro.au
+//#        Postal address: Malte Marquarding,
+//#                        Australia Telescope National Facility,
+//#                        P.O. Box 76,
+//#                        Epping, NSW, 2121,
+//#                        AUSTRALIA
+//#
+//# $Id:
+//#---------------------------------------------------------------------------
+
+#include "SDPol.h"
+
+#include <casa/Arrays/Array.h>
+#include <casa/Arrays/ArrayMath.h>
+#include <casa/Containers/Record.h>
+#include <casa/BasicSL/Constants.h>
+#include <casa/BasicSL/String.h>
+#include <casa/Utilities/Assert.h>
+
+#include <tables/Tables/Table.h>
+#include <tables/Tables/ScalarColumn.h>
+#include <tables/Tables/ArrayColumn.h>
+#include <tables/Tables/ColumnDesc.h>
+#include <tables/Tables/TableRecord.h>
+#include <tables/Tables/DataManError.h>
+
+
+using namespace casa;
+using namespace asap;
+
+
+
+SDStokesEngine::SDStokesEngine (const String& sourceColumnName,
+			   const String& targetColumnName)
+
+: BaseMappedArrayEngine<Float,Float> (sourceColumnName, targetColumnName)
+{}
+
+
+SDStokesEngine::SDStokesEngine (const Record& spec)
+: BaseMappedArrayEngine<Float,Float> ()
+{
+    if (spec.isDefined("SOURCENAME")  &&  spec.isDefined("TARGETNAME")) {
+        setNames (spec.asString("SOURCENAME"), spec.asString("TARGETNAME"));
+    }
+}
+
+SDStokesEngine::SDStokesEngine (const SDStokesEngine& that)
+: BaseMappedArrayEngine<Float,Float> (that)
+{}
+
+SDStokesEngine::~SDStokesEngine()
+{}
+
+
+DataManager* SDStokesEngine::clone() const
+{
+    DataManager* dmPtr = new SDStokesEngine (*this);
+    return dmPtr;
+}
+
+
+String SDStokesEngine::dataManagerType() const
+{
+    return className();
+}
+
+String SDStokesEngine::className()
+{
+    return "SDStokesEngine";
+}
+
+String SDStokesEngine::dataManagerName() const
+{
+    return sourceName();
+}
+
+Record SDStokesEngine::dataManagerSpec() const
+{
+    Record spec;
+    spec.define ("SOURCENAME", sourceName());
+    spec.define ("TARGETNAME", targetName());
+    return spec;
+}
+
+DataManager* SDStokesEngine::makeObject (const String&,
+						 const Record& spec)
+{
+    DataManager* dmPtr = new SDStokesEngine(spec);
+    return dmPtr;
+}
+
+
+void SDStokesEngine::registerClass()
+{
+    DataManager::registerCtor (className(), makeObject);
+}
+
+
+void SDStokesEngine::create (uInt initialNrrow)
+{
+    BaseMappedArrayEngine<Float,Float>::create (initialNrrow);
+}
+
+void SDStokesEngine::prepare()
+{
+    BaseMappedArrayEngine<Float,Float>::prepare();
+}
+
+Bool SDStokesEngine::canAccessArrayColumnCells (Bool& reask) const
+{
+    reask = False;
+    return True;
+}
+
+
+void SDStokesEngine::getArray (uInt rownr, Array<Float>& array)
+{
+    Array<Float> target(array.shape());
+    roColumn().get(rownr, target);
+//
+    computeOnGet (array, target);
+}
+
+void SDStokesEngine::putArray (uInt rownr, const Array<Float>& array)
+{
+    throw(AipsError("This Virtual Column is not writable"));
+}
+
+
+
+void SDStokesEngine::computeOnGet(Array<Float>& array,
+                   		 const Array<Float>& target)
+//
+// array of shape (nBeam,nIF,nPol,nChan)
+//
+{
+   DebugAssert(target.ndim()==4,AipsError);
+   DebugAssert(array.ndim()==4,AipsError);
+
+// The silly Array slice operator does not give me back
+// a const reference so have to caste it away
+
+   Array<Float>& target2 = const_cast<Array<Float>&>(target);
+
+// uInt polAxis = asap::PolAxis;
+   uInt polAxis = 2;
+//
+   const uInt nDim = target.ndim();
+   IPosition start(nDim,0);
+   IPosition end(target.shape()-1);
+
+// Input slices
+
+   start(polAxis) = 0;
+   end(polAxis) = 0;
+   Array<Float> C1 = target2(start,end);
+//
+   start(polAxis) = 1;
+   end(polAxis) = 1;
+   Array<Float> C2 = target2(start,end);
+//
+   start(polAxis) = 2;
+   end(polAxis) = 2;
+   Array<Float> C3 = target2(start,end);
+//
+   start(polAxis) = 3;
+   end(polAxis) = 3;
+   Array<Float> C4 = target2(start,end);
+
+// Compute Output slices
+
+   start(polAxis) = 0;
+   end(polAxis) = 0;
+   Array<Float> I = array(start,end); 
+   I = Float(0.5)*(C1 + C2);
+//
+   start(polAxis) = 1;
+   end(polAxis) = 1;
+   Array<Float> Q = array(start,end); 
+   Q = Float(0.5)*(C1 - C2);
+//
+   start(polAxis) = 2;
+   end(polAxis) = 2;
+   Array<Float> U = array(start,end); 
+   U = C3;
+//
+   start(polAxis) = 3;
+   end(polAxis) = 3;
+   Array<Float> V = array(start,end); 
+   V = C4;
+}
+
+
+
+
+// SDPolUtil
+
+Array<Float> SDPolUtil::polarizedIntensity (const Array<Float>& Q,
+                                            const Array<Float>& U)
+{
+   Array<Float> t1 = pow(Q,Double(2.0));
+   Array<Float> t2 = pow(U,Double(2.0));
+   return sqrt(t1+t2);
+}
+
+
+Array<Float> SDPolUtil::positionAngle (const Array<Float>& Q,
+                                       const Array<Float>& U)
+{
+   return Float(180.0/C::pi/2.0)*atan2(Q,U);
+}
+
+
+void SDPolUtil::rotateXYPhase (Array<Float>& C3,
+                               Array<Float>& C4,
+                               Float phase)
+{
+   Float cosVal = cos(C::pi/180.0*phase);
+   Float sinVal = sin(C::pi/180.0*phase);
+//
+   C3 = C3*cosVal - C4*sinVal;
+   C4 = C3*sinVal + C4*cosVal;
+}
+
+
+ 
---
+++ src/SDPol.h
@@ -0,0 +1,155 @@
+//#---------------------------------------------------------------------------
+//# SDPol.h: Polarimetric processing
+//#---------------------------------------------------------------------------
+//# Copyright (C) 2004
+//# ATNF
+//#
+//# This program is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU General Public License as published by the Free
+//# Software Foundation; either version 2 of the License, or (at your option)
+//# any later version.
+//#
+//# This program is distributed in the hope that it will be useful, but
+//# WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+//# Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with this program; if not, write to the Free Software Foundation, Inc.,
+//# 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning this software should be addressed as follows:
+//#        Internet email: Malte.Marquarding@csiro.au
+//#        Postal address: Malte Marquarding,
+//#                        Australia Telescope National Facility,
+//#                        P.O. Box 76,
+//#                        Epping, NSW, 2121,
+//#                        AUSTRALIA
+//#
+//# $Id:
+//#---------------------------------------------------------------------------
+#ifndef SDPOL_H
+#define SDPOL_H
+
+//# Includes
+#include <casa/aips.h>
+#include <casa/Arrays/Array.h>
+#include <tables/Tables/BaseMappedArrayEngine.h>
+
+
+namespace asap {
+
+class SDPolUtil
+{
+ public:
+// Convert Q and U to polarized intensity
+  static casa::Array<casa::Float> polarizedIntensity (const casa::Array<casa::Float>& Q,
+                                                      const casa::Array<casa::Float>& U);
+// Convert Q and U to polarized position angle (degrees)
+  static casa::Array<casa::Float> positionAngle (const casa::Array<casa::Float>& Q,
+                                                 const casa::Array<casa::Float>& U);
+// Rotate phase of Complex correlation C3+iC4 by phase (degrees)
+  static void rotateXYPhase (casa::Array<casa::Float>& C3,
+                             casa::Array<casa::Float>& C4,
+                             casa::Float phase);
+};
+
+
+
+class SDStokesEngine : public casa::BaseMappedArrayEngine<casa::Float, casa::Float>
+{
+  //# Make members of parent class known.
+public:
+  using casa::BaseMappedArrayEngine<casa::Float,casa::Float>::sourceName;
+protected:
+  using casa::BaseMappedArrayEngine<casa::Float,casa::Float>::targetName;
+  using casa::BaseMappedArrayEngine<casa::Float,casa::Float>::table;
+  using casa::BaseMappedArrayEngine<casa::Float,casa::Float>::roColumn;
+  using casa::BaseMappedArrayEngine<casa::Float,casa::Float>::rwColumn;
+
+public:
+    // Construct an engine to scale all arrays in a column with
+    // the given offset and scale factor.
+    // TargetColumnName is the name of the column where the scaled
+    // data will be put and must have data type Float.
+    // The source column using this engine must have data type Float.
+    SDStokesEngine (const casa::String& sourceColumnName,
+               const casa::String& targetColumnName);
+
+    // Construct from a record specification as created by getmanagerSpec().
+    SDStokesEngine (const casa::Record& spec);
+
+    // Destructor is mandatory.
+    ~SDStokesEngine();
+
+    // Return the type name of the engine (i.e. its class name).
+    virtual casa::String dataManagerType() const;
+
+    // Get the name given to the engine (is the source column name).
+    virtual casa::String dataManagerName() const;
+  
+    // casa::Record a casa::Record containing data manager specifications.
+    virtual casa::Record dataManagerSpec() const;
+
+    // Return the name of the class.
+    // This includes the names of the template arguments.
+    static casa::String className();
+
+   // The engine can access column cells.
+    virtual casa::Bool canAccessArrayColumnCells (casa::Bool& reask) const;
+
+    // Register the class name and the static makeObject "constructor".
+    // This will make the engine known to the table system.
+    // The automatically invoked registration function in DataManReg.cc
+    // contains SDStokesEngine
+    // Any other instantiation of this class must be registered "manually"
+    // (or added to DataManReg.cc).
+    static void registerClass();
+
+private:
+    // Copy constructor is only used by clone().
+    // (so it is made private).
+    SDStokesEngine (const SDStokesEngine&);
+
+    // Assignment is not needed and therefore forbidden
+    // (so it is made private and not implemented).
+    SDStokesEngine& operator=(const SDStokesEngine&);
+
+    // Clone the engine object.
+    DataManager* clone() const;
+
+    // Initialize the object for a new table.
+    // It defines the keywords containing the engine parameters.
+    void create (casa::uInt initialNrrow);
+
+    // Preparing consists of setting the writable switch and
+    // adding the initial number of rows in case of create.
+    // Furthermore it reads the keywords containing the engine parameters.
+    void prepare();
+
+    // Get an array in the given row.
+    void getArray (casa::uInt rownr, casa::Array<casa::Float>& array);
+
+    // Excpetion
+    void putArray (casa::uInt rownr, const casa::Array<casa::Float>& array);
+
+    // Compute STokes parameters
+    void computeOnGet (casa::Array<casa::Float>& array,
+    		     const casa::Array<casa::Float>& target);
+
+
+public:
+    //*display 4
+    // Define the "constructor" to construct this engine when a
+    // table is read back.
+    // This "constructor" has to be registered by the user of the engine.
+    // If the engine is commonly used, its registration can be added
+    // to the registerAllCtor function in DataManReg.cc. 
+    // That function gets automatically invoked by the table system.
+    static DataManager* makeObject (const casa::String& dataManagerType,
+				    const casa::Record& spec);
+};
+
+} // namespace
+
+#endif
