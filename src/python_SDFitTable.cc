aef58c4d3db90ed857bd978d7a7dee430512432c
mar637 <mar637@bac3dc5d-e65c-4774-9384-1203941d114d>
2005-02-17 09:48:48 +0000

    a class to transfer fit parameters.

---
+++ src/python_SDFitTable.cc
@@ -0,0 +1,54 @@
+//#---------------------------------------------------------------------------
+//# python_SDFitTable.cc: python exposure of c++ SDFitTable class
+//#---------------------------------------------------------------------------
+//# Copyright (C) 2004
+//# ATNF
+//#
+//# This program is free software; you can redistribute it and/or modify it
+//# under the terms of the GNU General Public License as published by the Free
+//# Software Foundation; either version 2 of the License, or (at your option)
+//# any later version.
+//#
+//# This program is distributed in the hope that it will be useful, but
+//# WITHOUT ANY WARRANTY; without even the implied warranty of
+//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+//# Public License for more details.
+//#
+//# You should have received a copy of the GNU General Public License along
+//# with this program; if not, write to the Free Software Foundation, Inc.,
+//# 675 Massachusetts Ave, Cambridge, MA 02139, USA.
+//#
+//# Correspondence concerning this software should be addressed as follows:
+//#        Internet email: Malte.Marquarding@csiro.au
+//#        Postal address: Malte Marquarding,
+//#                        Australia Telescope National Facility,
+//#                        P.O. Box 76,
+//#                        Epping, NSW, 2121,
+//#                        AUSTRALIA
+//#
+//# $Id:
+//#---------------------------------------------------------------------------
+#include <boost/python.hpp>
+
+#include "SDFitTable.h"
+
+using namespace boost::python;
+
+namespace asap {
+  namespace python {
+
+    void python_SDFitTable() {
+      class_<SDFitTable>("sdfit")
+        .def( init <> () )
+	.def( init < const SDFitTable& > () )
+        .def("__len__", &SDFitTable::STLlength)
+        .def("getfixedparameters", &SDFitTable::getSTLParameterMask)
+        .def("getparameters", &SDFitTable::getSTLParameters)
+        .def("getfunctions", &SDFitTable::getSTLFunctions)
+	.def("getcomponents", &SDFitTable::getSTLComponents)
+	.def("getframeinfo", &SDFitTable::getSTLFrameInfo)
+      ;
+    };
+
+  } //namespace python
+} // namespace asap
