43f55ed68020a648de5962bf213b8f376198c9e6
mar637 <mar637@bac3dc5d-e65c-4774-9384-1203941d114d>
2005-02-23 05:14:56 +0000

    added installation of contenst of bin directory

--- bin/install.sh
+++ bin/install.sh
@@ -9,35 +9,45 @@
 # temporary check to allow the same file for Narrabri and Epping
 if [ x"$NARRABRI_ASAP" = xyes ];
 then
-ASAPDIR='/DATA/KAPUTAR_2/vor010/ASAP/site-packages/asap'
-SRCDIR='/DATA/KAPUTAR_2/vor010/ASAP/asap'
+    ASAPDIR='/DATA/KAPUTAR_2/vor010/ASAP/site-packages/asap'
+    SRCDIR='/DATA/KAPUTAR_2/vor010/ASAP/asap'
+    BINDIR='/DATA/KAPUTAR_2/vor010/ASAP/'
 else
-ASAPDIR='/usr/local/lib/python2.3/site-packages/asap'
-SRCDIR='/u/mar637/brage/singledish/asap'
+    ASAPDIR='/usr/local/lib/python2.3/site-packages/asap'
+    SRCDIR='/u/mar637/asap'
+    BINDIR='/usr/local/bin'
 fi
 # 
-
 # check to allow setup via command line parameters
 if [ x"$1" != x ];
 then
-ASAPDIR=$1
-echo "ASAP will be installed into "$ASAPDIR
+    ASAPDIR=$1
+    echo "ASAP will be installed into "$ASAPDIR
 fi
 
 if [ x"$2" != x ];
 then
-SRCDIR=$2
-echo "from "$SRCDIR
+    SRCDIR=$2
+    echo "from "$SRCDIR
+fi
+
+if [ x"$3" != x ];
+then
+    BINDIR=$3
+    echo "Executables go into $BINDIR"
 fi
 
 # where the source python files are
 pydir='python'
 # where the library modules is
 libdir='lib'
+bindir='bin'
 # the python files to install
 srcfiles="__init__.py asapmath.py scantable.py asapreader.py asaplot.py asapfitter.py asapplotter.py asaplinefind.py"
+
 # the libraries to install
 libfiles='_asap.so'
+binfiles='asap'
 
 #if [ -d ${SHAREDIR} ] ; then
 #    echo "ASAP share dir already exists."
@@ -70,7 +80,11 @@ for f in ${libfiles}
 do
     cp -f ${SRCDIR}/${libdir}/${f} ${ASAPDIR}/
 done
-chmod -R g+w ${ASAPDIR}
+
+for f in ${binfiles}
+do
+    cp -f ${SRCDIR}/${bindir}/${f} ${BINDIR}/
+done
+
+#chmod -R g+w ${ASAPDIR}
 echo "Successfully installed the asap module into ${ASAPDIR}"
-
-
