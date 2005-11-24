8bff499124a468e943b74656b84bcbc3a7a5a702
mar637 <mar637@bac3dc5d-e65c-4774-9384-1203941d114d>
2005-11-24 03:07:03 +0000

    mopra test script

---
+++ test/mopra.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+from asap import *
+rcParams['verbose'] = 0
+print "Test using Mopra dual IF data..."
+print "Reduction test..."
+s = scantable("data/2005-05-08_0350.rpf")
+q = s.auto_quotient()
+q2 = quotient(s.get_scan("*_S"),s.get_scan("*_R"))
+del q2,s
+restfreqs = [110.201,86.243]     # 13CO-1/0, SiO the two IF
+q.set_restfreqs(restfreqs,"GHz") # set the restfrequencies, as not in data
+q.set_unit("km/s")               # set the unit to be used from now on
+q.set_freqframe("LSRK")          # set frequency frame
+q.auto_poly_baseline() # determine and subtract a poly baseline automatically
+q.convert_flux() # K -> Jy
+msk = q.create_mask([-70,20], [20,70]) # line free region - two windows
+rms = q.stats("rms",msk)
+med = q.stats("median",msk)
+#print q.summary()
+print "Fitter test..."
+f = fitter()
+f.set_scan(q)
+q.set_cursor(IF=0)
+f.set_function(gauss=2) # fit two gaussians
+f.fit()
+fp = f.get_parameters()
+print "Mopra Test successful"
