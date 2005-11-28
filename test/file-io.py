7528fc3a0206313e403151538ce537a5b90a1f07
phi196 <phi196@bac3dc5d-e65c-4774-9384-1203941d114d>
2005-11-28 05:49:22 +0000

    Test file i/o

---
+++ test/file-io.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from asap import *
+
+rcParams['verbose'] = 0
+
+
+print "Test of file i/o"
+
+data = scantable('data/tid-t002.rpf')
+
+data.save('output/test.asap',overwrite=True)
+data.save('output/test.fits',format='FITS',overwrite=True)
+data.save('output/test.ascii',format='ASCII',overwrite=True)
+data.save('output/test.sdfits',format='SDFITS',overwrite=True)
+
+data2 = scantable('output/test.sdfits')
+data3 = scantable('output/test.asap')
+
+print "File i/o test finished successfully"
