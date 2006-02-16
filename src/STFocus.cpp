5994dca9918abfcbebb55c30f6f3c397128292d3
mar637 <mar637@bac3dc5d-e65c-4774-9384-1203941d114d>
2006-02-16 01:03:28 +0000

    Focus subtable for Scantable. Initial revision.

---
+++ src/STFocus.cpp
@@ -0,0 +1,79 @@
+//
+// C++ Implementation: STFocus
+//
+// Description:
+//
+//
+// Author: Malte Marquarding <asap@atnf.csiro.au>, (C) 2006
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include <casa/Exceptions/Error.h>
+#include <tables/Tables/TableDesc.h>
+#include <tables/Tables/SetupNewTab.h>
+#include <tables/Tables/ScaColDesc.h>
+#include <tables/Tables/TableRecord.h>
+#include <tables/Tables/TableParse.h>
+#include <tables/Tables/TableRow.h>
+#include <casa/Containers/RecordField.h>
+
+#include "STFocus.h"
+
+
+using namespace casa;
+
+namespace asap {
+
+const casa::String STFocus::name_ = "FOCUS";
+
+STFocus::STFocus(casa::Table::TableType tt) :
+  STSubTable( name_, tt )
+{
+  setup();
+}
+
+
+STFocus::~STFocus()
+{
+}
+
+void asap::STFocus::setup( )
+{
+  // add to base class table
+  table_.addColumn(ScalarColumnDesc<Float>("ROTATION"));
+  table_.addColumn(ScalarColumnDesc<Float>("ANGLE"));
+  table_.addColumn(ScalarColumnDesc<Float>("TAN"));
+
+  // new cached columns
+  rotationCol_.attach(table_,"ROTATION");
+  angleCol_.attach(table_,"ANGLE");
+  tanCol_.attach(table_,"TAN");
+}
+
+uInt STFocus::addEntry( Float rotation, Float angle, Float ftan)
+{
+  Table result = table_( near(table_.col("ROTATION"), rotation)
+                    && near(table_.col("ANGLE"), angle)
+                    && near(table_.col("TAN"), ftan) );
+  uInt resultid = 0;
+  if ( result.nrow() > 0) {
+    ROScalarColumn<uInt> c(result, "ID");
+    c.get(0, resultid);
+  } else {
+    uInt rno = table_.nrow();
+    table_.addRow();
+    // get last assigned _id and increment
+    if ( rno > 0 ) {
+      idCol_.get(rno-1, resultid);
+      resultid++;
+    }
+    rotationCol_.put(rno, rotation);
+    angleCol_.put(rno, angle);
+    tanCol_.put(rno, ftan);
+    idCol_.put(rno, resultid);
+  }
+  return resultid;
+}
+
+}
---
+++ src/STFocus.h
@@ -0,0 +1,49 @@
+//
+// C++ Interface: STFocus
+//
+// Description:
+//
+//
+// Author: Malte Marquarding <asap@atnf.csiro.au>, (C) 2006
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef ASAPSTFOCUS_H
+#define ASAPSTFOCUS_H
+
+#include <casa/aips.h>
+#include <casa/BasicSL/String.h>
+#include <tables/Tables/Table.h>
+#include <tables/Tables/ScalarColumn.h>
+
+#include "STSubTable.h"
+
+namespace asap {
+
+/**
+The Focus subtable of the Scantable
+
+@author Malte Marquarding
+*/
+class STFocus : public STSubTable {
+public:
+  STFocus( casa::Table::TableType tt = casa::Table::Memory);
+
+  virtual ~STFocus();
+
+  casa::uInt addEntry( casa::Float rotation, casa::Float angle,
+                       casa::Float ftan);
+
+private:
+  void setup();
+  static const casa::String name_;
+  //casa::Table table_;
+  //casa::ScalarColumn<casa::uInt> freqidCol_;
+  casa::ScalarColumn<casa::Float> rotationCol_, angleCol_,
+                                  tanCol_;
+};
+
+}
+
+#endif
