f358e5670b17a4e2a4a067efa62fec709fb12a8f
mar637 <mar637@bac3dc5d-e65c-4774-9384-1203941d114d>
2006-02-17 02:39:22 +0000

    Forgot to change the contents in the last check in

--- src/Scantable.h
+++ src/Scantable.h
@@ -1,35 +1,16 @@
-//#---------------------------------------------------------------------------
-//# SDMemTable.h: A MemoryTable container for single dish integrations
-//#---------------------------------------------------------------------------
-//# Copyright (C) 2004
-//# ATNF
-//#
-//# This program is free software; you can redistribute it and/or modify it
-//# under the terms of the GNU General Public License as published by the Free
-//# Software Foundation; either version 2 of the License, or (at your option)
-//# any later version.
-//#
-//# This program is distributed in the hope that it will be useful, but
-//# WITHOUT ANY WARRANTY; without even the implied warranty of
-//# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
-//# Public License for more details.
-//#
-//# You should have received a copy of the GNU General Public License along
-//# with this program; if not, write to the Free Software Foundation, Inc.,
-//# 675 Massachusetts Ave, Cambridge, MA 02139, USA.
-//#
-//# Correspondence concerning this software should be addressed as follows:
-//#        Internet email: Malte.Marquarding@csiro.au
-//#        Postal address: Malte Marquarding,
-//#                        Australia Telescope National Facility,
-//#                        P.O. Box 76,
-//#                        Epping, NSW, 2121,
-//#                        AUSTRALIA
-//#
-//# $Id:
-//#---------------------------------------------------------------------------
-#ifndef SDMEMTABLE_H
-#define SDMEMTABLE_H
+//
+// C++ Interface: Scantable
+//
+// Description:
+//
+//
+// Author: Malte Marquarding <asap@atnf.csiro.au>, (C) 2005
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef ASAPSCANTABLE_H
+#define ASAPSCANTABLE_H
 
 // STL
 #include <string>
@@ -38,273 +19,282 @@
 #include <casa/aips.h>
 #include <casa/Arrays/MaskedArray.h>
 #include <casa/BasicSL/String.h>
+#include <casa/Utilities/CountedPtr.h>
+
 #include <coordinates/Coordinates/SpectralCoordinate.h>
+
 #include <tables/Tables/Table.h>
 #include <tables/Tables/ArrayColumn.h>
 #include <tables/Tables/ScalarColumn.h>
 
-#include "SDDefs.h"
-#include "SDFitTable.h"
+#include <measures/TableMeasures/ScalarMeasColumn.h>
+
 #include "SDLog.h"
+#include "SDContainer.h"
+#include "STFrequencies.h"
+#include "STWeather.h"
+#include "STFocus.h"
+#include "STTcal.h"
+#include "STMolecules.h"
+#include "STSelector.h"
+
+
 
 namespace asap {
 
-class SDContainer;
-class SDHeader;
-class SDFrequencyTable;
-class SDFitTable;
-
-
-class SDMemTable : private SDLog {
+/**
+  * This class contains and wraps a casa::Table, which is used to store
+  * all the information. This can be either a MemoryTable or a
+  * disk based Table.
+  * It provides access functions to the underlying table
+  * It contains n subtables:
+  * @li weather
+  * @li frequencies
+  * @li molecules
+  * @li tcal
+  * @li focus
+  * @li fits
+  *
+  * @brief The main ASAP data container
+  * @author Malte Marquarding
+  * @date
+  * @version
+*/
+class Scantable : private SDLog
+{
 public:
-  // create a new (empty) SDMemTable
-  SDMemTable();
-  // create a SDMemTable from an (aips++) table on disk
-  SDMemTable(const std::string& name);
+  /**
+   * Default constructor
+   */
+  Scantable(casa::Table::TableType ttype = casa::Table::Memory);
 
-  // Copy Construct a SDMemTable, if clear==True only header and
-  // skeleton are copied, otherwise the whole table is copied.
-  SDMemTable(const SDMemTable& other, casa::Bool clear=casa::False);
+  /**
+   * Create a Scantable object form an existing table on disk
+   * @param[in] name the name of the existing Scantable
+   */
+  Scantable(const std::string& name, casa::Table::TableType ttype = casa::Table::Memory);
 
-  // Copy Construct (copy semantics) a SDMemTable, give a scanid constraint
-  // see also getScan()
-  SDMemTable(const casa::Table& tab, const std::string& expr);
+  /// @fixme this is only sensible for MemoryTables....
+  Scantable(const Scantable& other, bool clear=true);
 
-  // Assignment operator (copy semantics)
-  SDMemTable &operator=(const SDMemTable& other);
+  /**
+   * Destructor
+   */
+  virtual ~Scantable();
 
-  virtual ~SDMemTable();
+  /**
+   * get a const reference to the underlying casa::Table
+   * @return const casa::Table reference
+   */
+  const casa::Table& table() const;
 
-  // put data from meta conatiner into the table
-  bool putSDContainer(const SDContainer& sdc);
-  bool putSDHeader(const SDHeader& sdh);
-  bool putSDFreqTable(const SDFrequencyTable& sdft);
+  /**
+   * get a reference to the underlying casa::Table with the Selection
+   * object applied if set
+   * @return casa::Table reference
+   */
+  casa::Table& table();
 
-  //get the data wrapped up in a meta container
+  void setSelection(const STSelector& selection);
+  void unsetSelection();
+  /**
+   * set the header
+   * @param[in] sdh an SDHeader object
+   */
+  void putSDHeader( const SDHeader& sdh );
 
-  SDContainer getSDContainer(casa::uInt whichRow=0) const;
-  SDHeader getSDHeader() const;
+  /**
+   * get the header information
+   * @return an SDHeader object
+   */
+  SDHeader getSDHeader( ) const;
 
-  // Get SD Frequency table.
-  SDFrequencyTable getSDFreqTable() const;
 
-  // get spectrum,mask and tsys for the given row, at the selected
-  // cursor - all as stl vectors
-  virtual std::vector<float> getSpectrum(casa::Int whichRow=0) const;
-  virtual std::vector<bool> getMask(casa::Int whichRow=0) const;
+  /**
+   * Checks if the "other" Scantable is conformant with this,
+   * i.e. if  header values are the same.
+   * @param[in] other another Scantable
+   * @return true or false
+   */
+  bool conformant( const Scantable& other);
 
-  // Get length of STokes spectrum. XX & YY -> I
-  // Anything else the length stays the same.
-  int nStokes() const;
+  /**
+   * return the number of scans in the table
+   * @return number of scans in the table
+   */
+  int nScan() const;
 
-  // Get specific Stokes at cursor location. One of either I,Q,U,V or
-  // I,P,PA,V (doPol=True) (determined by the polSel cursor location
-  // 0->3) If the latter, you can add a PA offset (degrees)
-  virtual std::vector<float> getStokesSpectrum(casa::Int whichRow=0,
-                                               casa::Bool doPol=casa::False) const;
+  //casa::MDirection::Types getDirectionReference() const;
+  //casa::MEpoch::Types getTimeReference() const;
 
-  // Returns Label for polarization selected by polSel_ (or
-  // over-ridden with arg. polIdx).  If doStokes is False, returns
-  // label for raw correlations (linear or circular). If True returns
-  // label for Stokes.  If doPol=False, I,Q,U,V else I,P,PA,V
-  std::string getPolarizationLabel(casa::Bool linear, casa::Bool stokes,
-                                   casa::Bool linPol,
-                                   casa::Int polIdx=-1) const;
+  /**
+   * Get global antenna position
+   * @return
+   */
+  casa::MPosition getAntennaPosition() const;
 
-  // Convert Stokes to linear or circular polarizations (specified by
-  // polIdx=0:3).  Presently, only conversion to RR or LL is
-  // supported.
-  virtual std::vector<float> stokesToPolSpectrum(casa::Int whichRow,
-                                                 casa::Bool toLinear,
-                                                 casa::Int polIdx) const;
+  /**
+   *
+   * @return
+   */
 
-  // Get all Stokes at the specified Beam/IF cursor location (ignoring
-  // the internal cursor).  -1 means all on that axis.  Really, this function
-  // should not be here, and the SDContainer should hold the stokes spectrum
-  // as well.  However, because the Stokes column is virtual, and its shape
-  // is not the same as nPol(), I have put it here for direct access as needed
-  virtual casa::Array<casa::Float> getStokesSpectrum(casa::Int whichRow=0,
-                                                     casa::Int iBeam=-1,
-                                                     casa::Int iIF=-1) const;
-
-  // Get Tsys at cursor location
-  virtual casa::Float getTsys(casa::Int whichRow=0) const;
-
-  // get all as aips++ Vectors
-  virtual void getSpectrum(casa::Vector<casa::Float>& spectrum,
-                           casa::Int whichRow=0) const;
-
-  // Get rest frequencies
-  std::vector<double> getRestFreqs() const;
-
-  // get info for current row
-  // if whichRow == -1 the Header time is given
-  std::string getTime(casa::Int whichRow=0,
-                      casa::Bool showDate=casa::False) const ;
-  casa::MEpoch getEpoch(casa::Int whichRow=0) const;
-  casa::MDirection getDirection(casa::Int whichRow=0,
-                                casa::Bool refBeam=casa::False) const;
-
-  std::string getSourceName(casa::Int whichRow=0) const;
-
-  float getElevation(casa::Int whichRow=0) const;
-  float getAzimuth(casa::Int whichRow=0) const;
-  float getParAngle(casa::Int whichRow=0) const;
-
-  double getInterval(casa::Int whichRow=0) const;
-
-  virtual void setSpectrum(std::vector<float> spectrum, int whichRow=0);
-  virtual void setCoordInfo(std::vector<string> theinfo);
-
-  // Set RestFreqID.  source="" and IF=-1 means select all
-  virtual casa::Bool setRestFreqs(const casa::Vector<casa::Double>& restFreqs,
-                                  const casa::String& unit,
-                                  const std::vector<std::string>& lines,
-                                  const casa::String& source,
-                                  casa::Int whichIF=-1);
-
-  // List lines
-  std::string spectralLines() const;
-
-  // Get/Set flux unit
   std::string getFluxUnit() const;
-  void setFluxUnit (const std::string& unit);
 
-  // Set Instrument
-  void setInstrument (const std::string& instrument);
+  /**
+   *
+   * @param unit
+   */
+  void setFluxUnit( const std::string& unit );
 
-  // set the current value
-  virtual bool setIF(casa::Int whichIF=0);
-  virtual bool setBeam(casa::Int whichBeam=0);
-  virtual bool setPol(casa::Int whichPol=0);
+  /**
+   *
+   * @param instrument
+   */
+  void setInstrument( const std::string& instrument );
 
-  // REset cursor to 0
-  virtual void resetCursor();
+  void calculateAZEL();
 
-  // Hard flags the current spectrum, not reversible
-  virtual void flag(int whichRow);
+  /**
+   * "hard" flags
+   * @param[in] whichrow
+   */
+  void flag();
 
-  // return the currently selected values
-  virtual casa::Int getIF() const { return IFSel_; }
-  virtual casa::Int getBeam() const { return beamSel_; }
-  virtual casa::Int getPol() const { return polSel_; }
+  int nbeam(int scanno=-1) const;
+  int nif(int scanno=-1) const;
+  int npol(int scanno=-1) const;
+  int nchan(int scanno=-1, int ifno=-1) const;
 
-  // returns unit, conversion frame, doppler, base-frame
-  virtual std::vector<std::string> getCoordInfo() const;
+  int nrow(int scanno=-1) const;
 
-  // number of scans in table
-  virtual casa::Int nScan() const;
+  double getInterval(int whichrow=0) const;
 
-  // get a summary of the table
-  virtual std::string summary(bool verbose=false) const;
+  float getTsys(int whichrow=0) const;
 
-  // get/set the history
+  std::vector<bool> getMask(int whichrow=0) const;
+  std::vector<float> getSpectrum(int whichrow=0) const;
+
+  std::vector<float> getStokesSpectrum( int whichrow=0,
+                                        bool dopol=false) const;
+  std::string getPolarizationLabel(bool linear, bool stokes,
+                                   bool linpol,
+                                   int polidx=-1) const;
+
+  void makePersistent(const std::string& filename);
+
+
+  void select(const STSelector& sel);
+
+  const STSelector& selection() const { return selector_; }
+
   std::vector<std::string> getHistory() const;
   void addHistory(const std::string& hist);
 
   casa::Table getHistoryTable() const;
   void appendToHistoryTable(const casa::Table& otherHist);
 
-  // write to disk as aips++ table
-  void makePersistent(const std::string& filename);
-
-  // get a new SDMemTable containing all rows with the same give SCANID
-  SDMemTable getScan(casa::Int scanID) const;
-  SDMemTable getSource(const std::string& source) const;
-
-  const casa::TableRecord& getHeader() const {return table_.keywordSet();}
-  // get a handle to the "raw" aips++ table
-  const casa::Table& table() const { return table_; }
-
-  // return the number of values
-  casa::Int nBeam() const;
-  casa::Int nIF() const;
-  casa::Int nPol() const;
-  casa::Int nChan() const;
-
-  // return the number of rows (integrations) in the table
-  casa::Int nRow() const { return table_.nrow(); }
-
-  // return a row as a Masked array, internally converting uChar flags
-  // to bool mask
-  casa::MaskedArray<casa::Float> rowAsMaskedArray(casa::uInt whichRow,
-                                                  casa::Bool toStokes=casa::False) const;
-
-  // Return SC, setting only the basic construction state (i.e.
-  // no conversion or velocity or rest frequency state).
-  // Specify the index of the FreqID you want
-  casa::SpectralCoordinate getSpectralCoordinate(casa::uInt whichIdx) const;
-
-  // Return SC. Set velocity conversion state (unit,doppler), and
-  // rest frequency.  If row number given (>=0), also set
-  // frame conversion layer (needs direction & time which require row)
-  casa::SpectralCoordinate getSpectralCoordinate(casa::uInt freqID,
-                                                 casa::uInt restFreqID,
-                                                 casa::uInt row) const;
-
-  // Set just the reference value, pixel and increment into the table
-  // No other state is extracted.
-  casa::Bool setCoordinate(const casa::SpectralCoordinate& speccord,
-                           casa::uInt whichIdx);
-
-  casa::Int nCoordinates() const;
-
-  std::vector<double> getAbcissa(int whichRow=0) const;
-  std::string getAbcissaString(casa::Int whichRow=0) const;
-
-// Get global reference  types
-  casa::MDirection::Types getDirectionReference() const;
-  casa::MEpoch::Types getTimeReference() const;
-
-// Get global antenna position
-  casa::MPosition getAntennaPosition() const;
-
-  bool putSDFitTable(const SDFitTable& sdft);
-  SDFitTable getSDFitTable() const;
-  SDFitTable getSDFitTable(casa::uInt whichRow) const;
-
-  void addFit(casa::uInt whichRow,
-              const casa::Vector<casa::Double>& p,
-              const casa::Vector<casa::Bool>& m,
-              const casa::Vector<casa::String>& f,
-              const casa::Vector<casa::Int>& c);
+  std::string summary(bool verbose=false);
+  std::string getTime(int whichrow=-1, bool showdate=true) const;
 
 
-  void calculateAZEL();
+  STFrequencies& frequencies() { return freqTable_; }
+  STWeather& weather() { return weatherTable_; }
+  STFocus& focus() { return focusTable_; }
+  STTcal& tcal() { return tcalTable_; }
+  STMolecules& molecules() { return moleculeTable_; }
 
 private:
-  // utility func for nice printout
-  casa::String formatSec(casa::Double x) const;
-  casa::String formatDirection(const casa::MDirection& md) const;
-  std::vector<float> getFloatSpectrum(const casa::Array<casa::Float>& arr) const;
-  void setup();
+  /**
+   * Turns a time vale into a formatted string
+   * @param x
+   * @return
+   */
+  std::string formatSec(casa::Double x) const;
+
+  std::string formatTime(const casa::MEpoch& me, bool showdate)const;
+
+  /**
+   *  Turns a casa::MDirection into a nicely formatted string
+   * @param md an casa::MDirection
+   * @return
+   */
+  std::string formatDirection(const casa::MDirection& md) const;
+
+
+  /**
+   * Create a unique file name for the paged (temporary) table
+   * @return just the name
+   */
+  static casa::String generateName();
+
+  /**
+   * attach to cached columns
+   */
   void attach();
-  void renumber();
 
-  // Generate start and end for shape and current cursor selection
-  void getCursorSlice(casa::IPosition& start, casa::IPosition& end,
-                      const casa::IPosition& shape) const;
+  /**
+   * Set up the main casa::Table
+   */
+  void setupMainTable();
 
-  // the current cursor into the array
-  casa::Int IFSel_,beamSel_,polSel_;
-  // the underlying memory table
+  void setupHistoryTable();
+  void setupMoleculeTable();
+  void setupFitTable();
+
+  /**
+   * Convert an "old" asap1 style row index into a new index
+   * @param[in] therow
+   * @return and index into @table_
+   */
+  int rowToScanIndex(int therow);
+
+  static const unsigned int version_ = 2;
+
+  STSelector selector_;
+
+  casa::Table::TableType type_;
+
+  // the actual data
   casa::Table table_;
-  // The current table version
-  static const casa::uInt version_ = 1;
+  casa::Table originalTable_;
+
+  STTcal tcalTable_;
+  STFrequencies freqTable_;
+  STWeather weatherTable_;
+  STFocus focusTable_;
+  STMolecules moleculeTable_;
+  casa::Table fitTable_;
+  casa::Table historyTable_;
 
   // Cached Columns to avoid reconstructing them for each row get/put
   casa::ScalarColumn<casa::Double> timeCol_, integrCol_;
-  casa::ScalarColumn<casa::Float> azCol_, elCol_, paraCol_;
-  casa::ScalarColumn<casa::String> srcnCol_, fldnCol_, tcaltCol_;
-  casa::ScalarColumn<casa::Int> scanCol_, rbeamCol_;
-  casa::ArrayColumn<casa::Float> specCol_, tsCol_, tcalCol_;
-  casa::ArrayColumn<casa::Double> dirCol_;
+  casa::MDirection::ScalarColumn dirCol_;
+  casa::ScalarColumn<casa::Double> azCol_;
+  casa::ScalarColumn<casa::Double> elCol_;
+  casa::ScalarColumn<casa::Float> paraCol_;
+  casa::ScalarColumn<casa::String> srcnCol_, fldnCol_;
+  casa::ScalarColumn<casa::uInt> scanCol_, beamCol_, cycleCol_;
+  casa::ScalarColumn<casa::Int> rbeamCol_;
+  casa::ArrayColumn<casa::Float> specCol_, tsCol_;
   casa::ArrayColumn<casa::uChar> flagsCol_;
-  casa::ArrayColumn<casa::uInt> freqidCol_, restfreqidCol_;
-  casa::ArrayColumn<casa::String> histCol_;
-  casa::ArrayColumn<casa::Int> fitCol_;
-  casa::ROArrayColumn<casa::Float> stokesCol_;
+
+  // id in frequencies table
+  casa::ScalarColumn<casa::uInt> mfreqidCol_;
+  // id in tcal table
+  casa::ScalarColumn<casa::uInt> mtcalidCol_;
+
+  casa::ArrayColumn<casa::String> histitemCol_;
+  casa::ScalarColumn<casa::uInt> mfitidCol_, fitidCol_;
+  // id in weather table and main table
+  casa::ScalarColumn<casa::uInt> mweatheridCol_;
+
+  casa::ScalarColumn<casa::uInt> mfocusidCol_;
+
+  casa::ScalarColumn<casa::uInt> mmolidCol_;
+
 };
 
-}// namespace
+
+} // namespace
+
 #endif
