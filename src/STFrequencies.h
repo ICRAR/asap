9a8f41e0c95987a2d82d467cc4d9a03f24cd2a4d
mar637 <mar637@bac3dc5d-e65c-4774-9384-1203941d114d>
2006-02-20 02:47:07 +0000

    documentation update

--- src/STFrequencies.h
+++ src/STFrequencies.h
@@ -26,6 +26,7 @@ namespace asap {
 The Frequencies subtable of the Scantable
 
 @author Malte Marquarding
+@brief The frequency subtable of the Scantable
 */
 class STFrequencies : public STSubTable {
 public:
@@ -42,34 +43,106 @@ public:
   casa::uInt addEntry( casa::Double refpix, casa::Double refval,
                        casa::Double inc );
 
+  /**
+   * Retrieve the frequency values for a specific id via references
+   * @param refpix the reference pixel
+   * @param refval the reference value
+   * @param inc the increment
+   * @param id the identifier
+   */
   void getEntry( casa::Double& refpix, casa::Double& refval,
                  casa::Double& inc, casa::uInt id );
 
+
+  /**
+   * Retrieve  the frequency values as a casa::SpectralCoordinate
+   * @param freqID
+   * @return casa::SpectralCoordinate
+   */
   casa::SpectralCoordinate getSpectralCoordinate( casa::uInt freqID );
 
+  /**
+   * Return the unit of the frequency values
+   * @return casa::Unit
+   */
   const casa::Unit& getUnit() const;
+  /**
+   * Return the doppler type of the values
+   * @return casa::MDoppler
+   */
   casa::MDoppler getDoppler() const;
 
 
+  /**
+   * Return the frame type, e.g MFrequency::TOPO
+   * @return casa::MFrequency::Types
+   */
   casa::MFrequency::Types getFrame() const;
+
+  /**
+   * Return a string representation of the frame type, e.g TOPO
+   * @return
+   */
   std::string getFrameString() const;
+
+  /**
+   * set the frequency frame from a string value
+   * @param frame a string identifier
+   */
   void setFrame(const std::string& frame);
+  /**
+   * set the frequency frame from a casa::MFrequency::Types
+   * @param frame casa::MFrequency::Types
+   */
   void setFrame(casa::MFrequency::Types frame);
 
+  /**
+   * rescale the whole table by a given factor
+   * @param factor the factor to bin or resample by
+   * @param mode the rescaling mode
+   * @li "BIN"
+   * @li "RESAMPLE"
+   */
   void rescale(casa::Float factor, const std::string& mode);
 
+  /**
+   * get the reference frequency at a given channel for a specidif identifier
+   * @param id the identifier
+   * @param channel the channel number
+   * @return teh reference frequency
+   */
   float getRefFreq(casa::uInt id, casa::uInt channel);
 
+
+  /**
+   * Rteun this table or s specific row as a string representation
+   * @param id the identifier. If id<0 all rows are returned
+   * @return a string
+   */
   std::string print(int id=-1);
 
 private:
+
+  /**
+   * setup the the column structure of the casa::table
+   */
   void setup();
+  /**
+   * the actual binning of the SpectralCoordinate as called by rescale
+   * @param sc
+   * @param factor the bin factor
+   * @return casa::SpectralCoordinate
+   */
   casa::SpectralCoordinate binCsys(const casa::SpectralCoordinate& sc, casa::Int factor);
+  /**
+   * the actual resampling of the SpectralCoordinate as called by rescale
+   * @param sc
+   * @param width the resacle width. Can be decimal.
+   * @return
+   */
   casa::SpectralCoordinate resampleCsys(const casa::SpectralCoordinate& sc, casa::Float width);
 
   static const casa::String name_;
-  //casa::Table table_;
-  //casa::ScalarColumn<casa::uInt> freqidCol_;
   casa::ScalarColumn<casa::Double> refvalCol_, refpixCol_, incrCol_;
 };
 
