a2c2871e32039814e3190cd152fe317bd3591cb9
mar637 <mar637@bac3dc5d-e65c-4774-9384-1203941d114d>
2006-03-31 04:35:43 +0000

    Class to transport fit parameters

---
+++ src/STFitEntry.cpp
@@ -0,0 +1,26 @@
+//
+// C++ Implementation: STFitEntry
+//
+// Description: 
+//
+//
+// Author: Malte Marquarding <Malte.Marquarding@csiro.au>, (C) 2006
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "STFitEntry.h"
+
+namespace asap {
+
+STFitEntry::STFitEntry()
+{
+}
+
+
+STFitEntry::~STFitEntry()
+{
+}
+
+
+}
