38cc73a43ec629627dfd93da883cab8349ed814d
mar637 <mar637@bac3dc5d-e65c-4774-9384-1203941d114d>
2006-08-02 03:16:16 +0000

    added documentation fixed ifndef

--- src/Logger.h
+++ src/Logger.h
@@ -1,27 +1,61 @@
-#ifndef SDLOG_H
-#define SDLOG_H
+#ifndef ASAPLOGGER_H
+#define ASAPLOGGER_H
 
 #include <string>
 #include <iostream>
 
 namespace asap {
-
+/**
+  * This class provides the logging within asap. All other classes which need to log
+  * should inherit from this.
+  * @brief The ASAP logging class
+  * @author Malte Marquarding
+  * @date $Date$
+  * @version
+  */
 class Logger {
 public:
+  /**
+   * Default Constructor
+   **/
   Logger();
-  virtual ~Logger();
+  
+  /**
+   * Constructor with switch to enable/disable logging
+   * @param[in] enabled indicating the deafult state
+   */  
   Logger(bool enabled);
+
+  /*
+   * Destructor
+   */
+  virtual ~Logger();
+  /**
+   * push another message into the logger
+   * @param[in] the message
+   * @param[in] whether to add a newline character at the end
+   */  
   void pushLog(const std::string& s, bool newline=true) const;
-  //void pushLog(const char* cs);
+  /**
+   * pop the message form the logger
+   * @returns the log message string
+   */  
   std::string popLog() const;
+  /**
+   * enable logging
+   */
   virtual void enableLog();
+  /**
+   * disable logging
+   */
   virtual void disableLog();
+  
 private:
   static std::string log_;
   bool enabled_;
 };
 
-}
+} // namespace
 
 #endif
 
