0ebec9751e301c9aadb8b88c68c0f2e71f561247
mar637 <mar637@bac3dc5d-e65c-4774-9384-1203941d114d>
2006-08-11 02:27:39 +0000

    changed to use scons environments

--- scons/casa.py
+++ scons/casa.py
@@ -1,47 +1,69 @@
-__revision__ = "$Version:$"
 import os
-import sys
-import platform
-from SCons.Script import *
+import re
 
-def addCasa(env):
-    casalibs = "casav atnf images ms components coordinates \
-                lattices fits measures measures_f \
-                tables scimath scimath_f casa wcs".split()
-    env.Prepend( LIBS =  casalibs )
-    casaincd = [os.path.join(env['CASAROOT'], 'code/include'), \
-                os.path.join(env['CASAROOT'], 'code/casa')]
-    env.Append( CPPPATH = casaincd )
-    casalibd = os.path.join(env['CASAROOT'], env['CASAARCH'], 'lib')
-    env.Append( LIBPATH = [ casalibd ] )
-    # Explicit templates in casa
-    env.Append( CPPFLAGS = ['-DAIPS_NO_TEMPLATE_SRC'] )
+def generate(env):
+    def CheckCasaLib(context, lib):
+        context.Message("Checking casa library '%s'..."%lib)
 
-def checkCasa(conf, path=None):
-    ''' look for casa libraries'''
-    conf.Message('Checking for casa libraries...')
-    casaarch = None
-    if os.environ.has_key('AIPSPATH'):
-        casa = os.environ.get('AIPSPATH').split()
-        conf.env.Append(CASAARCH = casa[1])
-        conf.env.Append(CASAROOT = casa[0])
-        addCasa(conf.env)
-        conf.Result('yes')
-        return True
-    casaarch = 'linux_gnu'
-    if sys.platform == 'darwin':
-        casaarch = 'darwin'
-    elif sys.platform == 'linux2' and platform.architecture()[0] == '64bit':
-        casaarch = 'linux_64b'
-    paths = "/nfs/aips++/weekly /aips++ /opt/aips++ ../casa_asap".split()
-    if path is not None and len(path):
-        paths = [path]
-    for p in paths:
-        if os.path.isfile(os.path.join(p, casaarch, "lib/libcasa.a")):
-            conf.env.Append(CASAARCH = casaarch)
-            conf.env.Append(CASAROOT = os.path.abspath(p))
-            addCasa(conf.env)
-            conf.Result('yes')
+        context.Result(r)
+        return r
+
+    def CheckCasa(context, path=None):
+        ''' look for casa libraries'''
+        def addCasa(env):
+            casalibs = "casav atnf images ms components coordinates \
+                        lattices fits measures measures_f \
+                        tables scimath scimath_f casa wcs".split()
+            #env.Prepend( LIBS =  casalibs )
+            casaincd = [os.path.join(env['CASAROOT'], 'code/include'), \
+                        os.path.join(env['CASAROOT'], 'code/casa')]
+            env.Append( CPPPATH = casaincd )
+            casalibd = os.path.join(env['CASAROOT'], env['CASAARCH'], 'lib')
+            env.Append( LIBPATH = [ casalibd ] )
+            # Explicit templates in casa
+            env.Append( CPPFLAGS = ['-DAIPS_NO_TEMPLATE_SRC'] )
+        context.Message('Checking for casa libraries...')
+        casaarch = None
+        if os.environ.has_key('AIPSPATH'):
+            casa = os.environ.get('AIPSPATH').split()
+            context.env.Append(CASAARCH = casa[1])
+            context.env.Append(CASAROOT = casa[0])
+            addCasa(context.env)
+            context.Result('yes')
             return True
-    conf.Result('no')
-    return False
+        casaarch = 'linux_gnu'
+        if sys.platform == 'darwin':
+            casaarch = 'darwin'
+        elif sys.platform == 'linux2' and platform.architecture()[0] == '64bit':
+            casaarch = 'linux_64b'
+        paths = "/nfs/aips++/weekly /aips++ /opt/aips++ ../casa_asap".split()
+        if path is not None and len(path):
+            paths = [path]
+        # @todo poor mans detection, do autocontext later
+        for p in paths:
+            if os.path.isfile(os.path.join(p, casaarch, "lib/libcasa.a")):
+                context.env.Append(CASAARCH = casaarch)
+                context.env.Append(CASAROOT = os.path.abspath(p))
+                addCasa(context.env)
+                context.Result('yes')
+                return True
+        context.Result('no')
+        return False
+
+
+    def AddCustomTests(conf):
+        conf.AddTests({
+                        'CheckCasa'            : CheckCasa,
+                      })
+
+    env.AddCustomTests = AddCustomTests
+
+    def AddCustomPath(path=""):
+        if not len(path) or not os.path.exists(path):
+            return
+        env.PrependUnique(CPPPATH = [os.path.join(path, "include")])
+        env.PrependUnique(LIBPATH = [os.path.join(path, "lib")])
+    env.AddCustomPath = AddCustomPath
+
+def exists(env):
+    return true
--- scons/installtree.py
+++ scons/installtree.py
@@ -1,30 +1,35 @@
 import os
 import fnmatch
-from SCons.Script import *
+#from SCons.Script import *
 
-def InstallTree(env, dest_dir, src_dir, includes, excludes):
-    destnode = env.Dir(dest_dir)
-    dirs = []
-    dirs.append(src_dir)
-    while len(dirs) > 0:
-        currdir = dirs.pop(0)
-        currdestdir = dest_dir + currdir[len(src_dir):]
-        flist = os.listdir(currdir)
-        for currfile in flist:
-            currpath = os.path.join(currdir, currfile)
-            match = 0
-            for pattern in includes:
-                if fnmatch.fnmatchcase(currfile, pattern):
-                    match = 1
-            if (match == 1):
-                for pattern in excludes:
+def generate(env):
+    def InstallTree(dest_dir, src_dir, includes, excludes):
+        destnode = env.Dir(dest_dir)
+        dirs = []
+        dirs.append(src_dir)
+        while len(dirs) > 0:
+            currdir = dirs.pop(0)
+            currdestdir = dest_dir + currdir[len(src_dir):]
+            flist = os.listdir(currdir)
+            for currfile in flist:
+                currpath = os.path.join(currdir, currfile)
+                match = 0
+                for pattern in includes:
                     if fnmatch.fnmatchcase(currfile, pattern):
-                        match = 0
+                        match = 1
                 if (match == 1):
-                    if (os.path.isdir(currpath)):
-                        #print "d=" + currpath
-                        dirs.append(currpath)
-                    else:
-                        #print "f=" + currpath
-                        env.Install(currdestdir, currpath)
-    return destnode
+                    for pattern in excludes:
+                        if fnmatch.fnmatchcase(currfile, pattern):
+                            match = 0
+                    if (match == 1):
+                        if (os.path.isdir(currpath)):
+                            #print "d=" + currpath
+                            dirs.append(currpath)
+                        else:
+                            #print "f=" + currpath
+                            env.Install(currdestdir, currpath)
+        return destnode
+
+    env.InstallTree = InstallTree
+def exists(env):
+    return true
