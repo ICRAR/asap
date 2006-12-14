75727cc1a9393ac9da5ed22867f64fdb199bbc58
mar637 <mar637@bac3dc5d-e65c-4774-9384-1203941d114d>
2006-12-14 00:22:52 +0000

    version set to trunk

--- python/__init__.py
+++ python/__init__.py
@@ -357,7 +357,7 @@ if rcParams['useplotter']:
 	print "Matplotlib not installed. No plotting available"
 
 __date__ = '$Date$'.split()[1]
-__version__  = '2.1.1'
+__version__  = 'trunk'
 
 def is_ipython():
     return '__IP' in dir(sys.modules["__main__"])
