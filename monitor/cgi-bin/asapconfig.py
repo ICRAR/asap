03eb732f64ca7c667655a67106f37281a458ab62
MalteMarquarding <MalteMarquarding@bac3dc5d-e65c-4774-9384-1203941d114d>
2008-01-31 04:01:00 +0000

    removed some hardcoded locations

--- monitor/cgi-bin/asapconfig.py
+++ monitor/cgi-bin/asapconfig.py
@@ -5,16 +5,11 @@ observatory = {'rpfpath': [],
                'lines' : {} }
 
 import os,sys
-# This is where asap lives
-sys.path.insert(2,'/opt/lib/python2.3/site-packages')
-os.environ["AIPSPATH"]="/opt/share/asap linux_gnu somewhere localhost"
-
-#overwrite /usr/local/... as default
-sys.path.insert(2,'/usr/lib/python2.3/site-packages')
+#os.environ["AIPSPATH"]="/opt/share/asap linux_gnu somewhere localhost"
 
 # This is needed for plotting with matplotlib
 # where matplotlib data is located
-os.environ["MATPLOTLIBDATA"]="/opt/share/matplotlib"
+#os.environ["MATPLOTLIBDATA"]="/opt/share/matplotlib"
 # where matplotlib puts it temporary font files
 # this location can also have a custom .matplotlibrc
 os.environ["HOME"]="/var/www/asapmon/tmp"
