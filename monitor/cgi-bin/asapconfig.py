edc94f7903b25775fcae239ced04831dbb44f853
MalteMarquarding <MalteMarquarding@bac3dc5d-e65c-4774-9384-1203941d114d>
2008-05-21 05:35:49 +0000

    some updates to make the asapmonitor work again

--- monitor/cgi-bin/asapconfig.py
+++ monitor/cgi-bin/asapconfig.py
@@ -4,12 +4,9 @@ observatory = {'rpfpath': [],
                'name': 'observatory',
                'lines' : {} }
 
+asapmonhome = "/var/www/htdocs"
+asapmonrel = "/asapmon/"
 import os,sys
-#os.environ["AIPSPATH"]="/opt/share/asap linux_gnu somewhere localhost"
-
-# This is needed for plotting with matplotlib
-# where matplotlib data is located
-#os.environ["MATPLOTLIBDATA"]="/opt/share/matplotlib"
 # where matplotlib puts it temporary font files
 # this location can also have a custom .matplotlibrc
-os.environ["HOME"]="/var/www/asapmon/tmp"
+os.environ["HOME"]="/var/www/htdocs/asapmon/tmp"
