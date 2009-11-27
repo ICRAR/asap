08b27273d5d628cbd25bdb7aa09f3cacaee9def8
MalteMarquarding <MalteMarquarding@bac3dc5d-e65c-4774-9384-1203941d114d>
2009-11-27 05:03:39 +0000

    Added asap version 2 to 3 conversion program

---
+++ apps/asap2to3.cpp
@@ -0,0 +1,33 @@
+#include <casa/aips.h>
+#include <casa/iostream.h>
+#include <casa/Inputs.h>
+#include <tables/Tables/Table.h>
+#include <tables/Tables/ScaColDesc.h>
+
+//#include <casa/namesapce.h>
+using namespace casa;
+
+int main(int argc, const char* argv[]) {
+  // enable input in no-prompt mode
+  Input inputs(1);
+  // define the input structure
+  inputs.version("20090915MM");
+  inputs.create ("in", "", "Name of asap file", "string");
+  // Fill the input structure from the command line.
+  inputs.readArguments (argc, argv);
+  String fname =  inputs.getString("in");
+  Table origtab(fname);
+  fname +=".asap3";
+  origtab.deepCopy(fname, Table::New);
+  Table tab(fname, Table::Update);
+  tab.removeColumn("PARANGLE");
+  Table tfocus = tab.rwKeywordSet().asTable("FOCUS");
+  ScalarColumnDesc<Float> pa("PARANGLE");
+  pa.setDefault(Float(0.0));
+  tfocus.addColumn(pa);
+  cout << "WARNING: This has invalidated the parallactic angle in the data. Reprocess the data in ASAP 3 "
+       << "if you need to handle polarisation conversions"
+       << endl;
+  return 0;
+  
+}
