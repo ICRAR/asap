e988873ca2652d7709f4ea44a6f6d3546509fe32
TakeshiNakazato <TakeshiNakazato@bac3dc5d-e65c-4774-9384-1203941d114d>
2011-03-31 04:44:44 +0000

    New Development: No             JIRA Issue: Yes CAS-2718         Ready for Test: Yes      Interface Changes: No What Interface Changed: Please list interface changes          Test Programs: List test programs   Put in Release Notes: Yes/No              Module(s): Module Names change impacts.            Description: Describe your changes here...
    Fixed wrong reference pixel number in FREQUENCIES subtable.

--- src/MSFiller.cpp
+++ src/MSFiller.cpp
@@ -585,8 +585,7 @@ void MSFiller::fill()
           }
           Int refchan = nchan / 2 ;
           IPosition refip( 1, refchan ) ;
-          //Double refpix = 0.5*(nchan-1) ;
-          Double refpix = 0.5*(nchan) ;
+          Double refpix = 0.5*(nchan-1) ;
           Double refval = 0.0 ;
           sharedQDArrCol = new ROArrayQuantColumn<Double>( spwtab, "CHAN_WIDTH" ) ;
           Double increment = (*sharedQDArrCol)( spwId )( refip ).getValue( "Hz" ) ;
