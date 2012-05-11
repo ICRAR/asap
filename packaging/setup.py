f726af64b24011a53dd2e4f614eb17b02688ac23
MalteMarquarding <MalteMarquarding@bac3dc5d-e65c-4774-9384-1203941d114d>
2012-05-11 05:25:52 +0000

    use ipython >=0.11

--- packaging/setup.py
+++ packaging/setup.py
@@ -19,10 +19,10 @@ This package provides reduction and analysis tools for single-dish
 spectral-line data.
 ''',
       packages = find_packages(),
-      install_requires = ["ipython<=0.10", "matplotlib>=0.99", "numpy>=1.3"],
+      install_requires = ["ipython>=0.11", "matplotlib>=0.99", "numpy>=1.3"],
       package_data = {"": ["_asap.so", "data/ipy*"],
                       },
-      scripts = ["bin/asap", "bin/asap2to3", "bin/asap_update_data"],
+      scripts = ["bin/asap", "bin/asap_update_data"],
       license = 'GPL',
       zip_safe = 0,
       ext_modules = [ ext ], # dummy (see above)
