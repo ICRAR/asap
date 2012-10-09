f3de6ac4364ea1190fc038c7b0d37a2140bf9d79
MalteMarquarding <MalteMarquarding@bac3dc5d-e65c-4774-9384-1203941d114d>
2012-10-09 01:05:47 +0000

    create temporary 'sudo' MPLCFGDIR

--- install_osx.sh
+++ install_osx.sh
@@ -8,7 +8,8 @@ if [ `id -u` -ne 0 ]; then
 fi
 ASAPVERS=4.1
 OSXVERS=$(sw_vers |  grep -o '10\.[7-8]')
-if [ -n ${OSXVERS} ]; then echo "Only OS X >= 10.7 supported"; exit 1; fi
+echo $OSXVERS
+if [ -z ${OSXVERS} ]; then echo "Only OS X >= 10.7 supported"; exit 1; fi
 ASAPEGG="${FTPURL}/${ASAPVERS}/asap-4.1.0-py2.7-macosx-${OSXVERS}-intel.egg"
 MPLEGG="${FTPURL}/matplotlib/matplotlib-1.1.1-py2.7-macosx-${OSXVERS}-intel.egg"
 MPL=$(/usr/bin/python -c 'import matplotlib' >/dev/null 2>&1)
@@ -16,14 +17,17 @@ MPL=$(/usr/bin/python -c 'import matplotlib' >/dev/null 2>&1)
 if [ -n $? ]
 then
     echo "Matplotlib not installed. Installing from CASS ftp server..."
-    /usr/bin/easy_install-2.7 ${MPLEGG}
+    /usr/bin/easy_install-2.7 -U ${MPLEGG}
 fi
 
 # This should pull in IPython if required
-/usr/bin/easy_install-2.7 ${ASAPEGG}
+/usr/bin/easy_install-2.7 -U ${ASAPEGG}
 
 # make sure readline is installed
 /usr/bin/easy_install-2.7 readline
 
 # update measures data
-/usr/local/bin/asap_update_data
+MPLTMP=/tmp/mpltmp
+mkdir ${MPLTMP}
+MPLCONFIGDIR="${MPLTMP}" /usr/local/bin/asap_update_data
+rm -rf ${MPLTMP}
