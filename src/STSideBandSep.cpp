e6a298594a0d46a138a6bfd715b884c764bb4c35
KanaSugimoto <KanaSugimoto@bac3dc5d-e65c-4774-9384-1203941d114d>
2013-09-13 10:42:22 +0000

    Yet another attempt to fix build failure of Jenkins

--- src/STSideBandSep.cpp
+++ src/STSideBandSep.cpp
@@ -15,6 +15,7 @@
 // cascore
 #include <casa/OS/File.h>
 #include <casa/Logging/LogIO.h>
+#include <casa/Arrays/Vector.h>
 #include <casa/Quanta/QuantumHolder.h>
 
 #include <measures/Measures/MFrequency.h>
