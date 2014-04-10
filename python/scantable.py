c21bfee90854eb56b2d18adbdc064d112b58dc24
WataruKawasaki <WataruKawasaki@bac3dc5d-e65c-4774-9384-1203941d114d>
2014-04-10 06:27:07 +0000

    New Development: No             JIRA Issue: No         Ready for Test: Yes      Interface Changes: No What Interface Changed:          Test Programs:   Put in Release Notes: No              Module(s): sd            Description: modified an error message from get_first_rowno_by_if().

--- python/scantable.py
+++ python/scantable.py
@@ -2170,7 +2170,7 @@ class scantable(Scantable):
                 found = True
                 break
 
-        if not found: raise RuntimeError("Invalid IF value.")
+        if not found: raise RuntimeError("No valid spw.")
         
         return res
 
