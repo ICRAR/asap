75f94fbf663027a6905583d50ba6a67e043d885a
MalteMarquarding <MalteMarquarding@bac3dc5d-e65c-4774-9384-1203941d114d>
2014-04-17 01:34:45 +0000

    Make ipython >=1.x compatible

--- python/__init__.py
+++ python/__init__.py
@@ -59,7 +59,7 @@ from sbseparator import sbseparator
 from _asap import srctype
 
 __date__ = '$Date$'.split()[1]
-__version__  = '4.2.0a'
+__version__  = '4.3.0a'
 __revision__ = get_revision()
 
 def welcome():
